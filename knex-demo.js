292a23460dda742220f43020959bfd0c72240a33
Lukas Horak <lukasbhorak@gmail.com>
2019-06-18 14:49:35 -0400

    implemented query exerise with knex

---
+++ knex-demo.js
@@ -0,0 +1,37 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+// Store name in 'arg' variable for convenience later
+const arg = process.argv[2];
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: settings.hostname,
+    user: settings.user,
+    password: settings.password,
+    database: settings.database,
+    port: settings.port
+  }
+});
+
+knex.select("first_name", "last_name", knex.raw("to_char(birthdate, 'YYYY-MM-DD') as birthdate"))
+  .where("first_name", "like", `%${arg}%`)
+  .orWhere("last_name", "like", `%${arg}%`)
+  .from("famous_people")
+  .then(function (res) {
+      const numResults = res.length;
+      if (numResults > 0) {
+        const people = (numResults > 1 ? "people" : "person");
+        let returnString = `Found ${numResults} ${people} by the name '${arg}:`;
+        let count = 0;
+        for (let person of res) {
+          count += 1;
+          returnString += `\n- ${count}: ${person.first_name} ${person.last_name}, born '${person.birthdate}'`
+        }
+        console.log(returnString);
+      }
+      })
+    .finally(() => {
+      knex.destroy();
+    })
